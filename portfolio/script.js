5f90430e58b5b3d7e228824473b8ae1325febd31
Surya7914 <86120575+Surya7914@users.noreply.github.com>
2022-05-23 12:03:06 +0530

    updated

--- portfolio/script.js
+++ portfolio/script.js
@@ -74,3 +74,39 @@ $(document).ready(function(){
 function websiteVisits(response){
     document.querySelector("#visits").textContent=response.value;
 }
+const KEY = `023044b2-5620-4e89-93cb-a54e2f883d47`;
+const NAMESPACE = "default";
+const COUNT_URL = `https://api.countapi.xyz`;
+
+const counter = document.querySelectorAll('span');
+
+const getCount = async () => {
+  const response = await fetch(`${COUNT_URL}/get/${NAMESPACE}/${KEY}`);
+  const data = await response.json();
+    setValue(data.value);
+};
+
+const incrementCount = async () => {
+  const response = await fetch(`${COUNT_URL}/hit/${NAMESPACE}/${KEY}`);
+  const data = await response.json();
+  setValue(data.value);
+};
+
+const setValue = (num) => {
+  var str = num.toString().padStart(6, "0");
+  for (let index = 0; index < str.length; index++) {
+    const element = str[index];
+    counter[index].innerHTML = element;
+  }
+};
+
+if (localStorage.getItem("hasVisited") == null) {
+  incrementCount()
+    .then(() => {
+      localStorage.setItem("hasVisited", "true");
+    })
+    .catch((err) => console.log(err));
+} else {
+  getCount()
+    .catch((err) => console.log(err));
+}
